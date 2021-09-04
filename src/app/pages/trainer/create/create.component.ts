8d974072ba27d8f6b608ae9f18df3468e52404d4
Sergio Mallea <sergiomallea80@gmail.com>
2021-09-03 21:18:05 -0400

    creacion de servicios para post en el backend

--- src/app/pages/trainer/create/create.component.ts
+++ src/app/pages/trainer/create/create.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormGroupName, Validators } from '@angular/forms';
 import { SimpleModalService } from 'ngx-simple-modal';
 import { ImageCropperComponent } from 'src/app/components/image-cropper/image-cropper.component';
+import { LectionService } from 'src/app/services/lection.service';
+import { TrainerService } from 'src/app/services/trainer.service';
 
 @Component({
   selector: 'app-trainer-create',
@@ -13,25 +15,53 @@ export class CreateComponent implements OnInit {
   portraitImage: any;
   courseImage: any;
 
-  trainerForm: FormGroup;
-  lectionForm: FormGroup;
+  trainer: any = {};
+  lection: any = {};
 
   constructor(
-    private simpleModalService: SimpleModalService
+    private simpleModalService: SimpleModalService,
+    private trainerService: TrainerService,
+    private lectionService: LectionService
   ) { }
 
   ngOnInit(): void {
-    this.createForm();
+    this.createTrainerForm();
+    this.createLectionForm();
   }
   post(){
-    console.log(this.trainerForm.value);
+    this.trainer.value.image = this.iconImage;
+    this.trainer.value.imageFront = this.portraitImage;
+    this.lection.value.image = this.courseImage;
+
+    this.trainerService.post(this.trainer.value).subscribe((data :any ) => {      
+      console.log(data);
+      this.lection.value.trainerId = data.id;
+      this.lectionService.post(this.lection.value).subscribe(lectionData =>{  
+      });
+      //lectionService
+      //redirrecionar a la lista al terminar  
+      //averiguar Spinner    
+    });
   }
   
-  createForm(){
-    this.trainerForm = new FormGroup({
+  createTrainerForm(){
+    this.trainer = new FormGroup({
       names: new FormControl('',Validators.required),
       description: new FormControl('',Validators.required),
+      image: new FormControl(''),
+      imageFront: new FormControl(''),
       socialMedia: new FormControl('')
+    });
+  }
+  createLectionForm(){
+    this.lection= new FormGroup({
+      trainerId: new FormControl(),
+      name: new FormControl('',Validators.required),
+      description: new FormControl('',Validators.required),
+      mode: new FormControl('',Validators.required),
+      image: new FormControl(''),
+      titleGratitude: new FormControl('',Validators.required),
+      descriptionGratitude: new FormControl('',Validators.required)
     });
   }
 
---
+++ src/app/services/lection.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LectionService {
+  constructor(private http: HttpClient) {}
+
+  post(body) {
+      return this.http.post(`${environment.apiUrl}lections`, body).pipe();
+  }
+}
