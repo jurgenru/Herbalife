2e468d36f7e8adfa09cb647f94f868e87cb0a926
jurgenru <jurgenruegnberg@gmail.com>
2021-09-08 10:37:15 -0400

    create edit llist components for test,conten&servi

--- src/app/pages/service/service.routing.ts
+++ src/app/pages/service/service.routing.ts
@@ -1,31 +1,39 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { CreateComponent } from './create/create.component';
-import { ListComponent } from './list/list.component';
-import { ServiceComponent } from './service.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { CreateComponent } from "./create/create.component";
+import { EditComponent } from "./edit/edit.component";
+import { ListComponent } from "./list/list.component";
+import { ServiceComponent } from "./service.component";
 
-const routes: Routes = [{
-  path: '',
-  component: ServiceComponent,
+const routes: Routes = [
+  {
+    path: "",
+    component: ServiceComponent,
     children: [
       {
-        path: 'create',
-        component: CreateComponent
+        path: "create",
+        component: CreateComponent,
       },
       {
-        path: 'list',
-        component: ListComponent
-      }
-  ]
-}];
+        path: "edit",
+        component: EditComponent,
+      },
+      {
+        path: "list",
+        component: ListComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ServiceRoutingModule { }
+export class ServiceRoutingModule {}
 export const routedComponents = [
   ServiceComponent,
   CreateComponent,
-  ListComponent
+  EditComponent,
+  ListComponent,
 ];
--- src/app/pages/testimony/testimony.routing.ts
+++ src/app/pages/testimony/testimony.routing.ts
@@ -1,25 +1,39 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { CreateComponent } from './create/create.component';
-import { TestimonyComponent } from './testimony.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { CreateComponent } from "./create/create.component";
+import { EditComponent } from "./edit/edit.component";
+import { ListComponent } from "./list/list.component";
+import { TestimonyComponent } from "./testimony.component";
 
-const routes: Routes = [{
-  path: '',
-  component: TestimonyComponent,
-  children: [
-    {
-      path: 'create',
-      component: CreateComponent
-    }
-  ]
-}];
+const routes: Routes = [
+  {
+    path: "",
+    component: TestimonyComponent,
+    children: [
+      {
+        path: "create",
+        component: CreateComponent,
+      },
+      {
+        path: "edit",
+        component: EditComponent,
+      },
+      {
+        path: "list",
+        component: ListComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class TestimonyRoutingModule { }
+export class TestimonyRoutingModule {}
 export const routedComponents = [
   TestimonyComponent,
-  CreateComponent
+  CreateComponent,
+  EditComponent,
+  ListComponent,
 ];
