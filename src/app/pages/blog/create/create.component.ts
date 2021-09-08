2b0671b8d0867ffc8d464bb81d89a1cebd9c049c
Sergio Mallea <sergiomallea80@gmail.com>
2021-09-07 23:20:09 -0400

    se crearon los servicios para blog y article

--- src/app/pages/blog/create/create.component.ts
+++ src/app/pages/blog/create/create.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { SimpleModalService } from 'ngx-simple-modal';
+import { ToastrService } from 'ngx-toastr';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { ImageCropperComponent } from 'src/app/components/image-cropper/image-cropper.component';
+import { ArticleService } from 'src/app/services/article.service';
+import { BlogService } from 'src/app/services/blog.service';
 
 @Component({
   selector: 'app-create',
@@ -14,12 +19,15 @@ export class CreateComponent implements OnInit {
   iconImage: any;
   portraitImage: any;
   articleImage: any = [];
-  articleImage1: any;
-  articleImage2: any;
   
   constructor(
     private simpleModalService: SimpleModalService,
     private formBuilder: FormBuilder,
+    private blogService: BlogService,
+    private articleService: ArticleService,
+    private router: Router,
+    private spinner: NgxUiLoaderService,
+    private toastr: ToastrService
 
   ) { }
 
@@ -76,21 +84,44 @@ export class CreateComponent implements OnInit {
   }
   
   post(){
+    const start = new Date();
+    this.spinner.start();
+    this.blog.value.userId = "1";
     this.blog.value.icon = this.iconImage;
     this.blog.value.image = this.portraitImage;
-    console.log(this.blog.value);
-    console.log(this.articleForm.value);
+    
+    this.blogService.post(this.blog.value).subscribe((data: any) =>{
+      console.log(data);
+      this.article.controls.forEach((element) => {
+        element.value.blogId = data.id;
+        this.articleService.post(element.value).subscribe((articleData:any)=>{
+          console.log(articleData);
+        });
+      });
+      const end = new Date();
+          const elapsed = (end.getSeconds() - start.getSeconds()) * 1000;
+          setTimeout(() => {
+            this.spinner.stop();
+           // this.router.navigate(['/blog/list'])
+            this.notification('<span class="tim-icons icon-bell-55" [data-notify]="icon"></span> Se ha creado el blog', '5000', 'success', 'top', 'center');
+            
+          }, elapsed);
+    });
+  }
+
+  notification(content, time, type, from, align) {
+    this.toastr.error(content, '', {
+      timeOut: time,
+      closeButton: true,
+      enableHtml: true,
+      toastClass: `alert alert-${type} alert-with-icon`,
+      positionClass: 'toast-' + from + '-' +  align
+    });
   }
   get article(): FormArray {
     return this.articleForm.get('article') as FormArray;
   }
   get name(){
     return this.blog.get("name");
-  }
-  get title(){
-    return this.article.get('title');
-  }
-  get description(){
-    return this.article.get('description');
   }
 }
