dcc9d5a29c0f9f727016e03bb989ab51334a0cfb
Noelia Martínez <noeowo384@gmail.com>
2021-09-09 17:55:48 -0400

    Blog: modificando presentación edit

--- src/app/pages/blog/edit/edit.component.ts
+++ src/app/pages/blog/edit/edit.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { BlogService } from 'src/app/services/blog.service';
+import { ImageCropperComponent } from 'src/app/components/image-cropper/image-cropper.component';
+import { SimpleModalService } from 'ngx-simple-modal';
 
 @Component({
   selector: 'app-blog-edit',
@@ -11,9 +14,14 @@ export class EditComponent implements OnInit {
 
   blog: any = {};
   articles: any;
+  iconImage: any;
+  portraitImage: any;
+  articleImage: any = [];
+  articleForm: any = {};
 
   constructor(
     private blogService: BlogService,
+    private simpleModalService: SimpleModalService,
     private route: ActivatedRoute,
   ) {
     this.route.params.subscribe(val => {
@@ -27,5 +35,22 @@ export class EditComponent implements OnInit {
   }
 
   ngOnInit(): void { }
+
+  showIcon() {
+    this.simpleModalService.addModal(ImageCropperComponent).subscribe((data) => {
+      this.iconImage = data;
+    });
+  }
+  showPortrait() {
+    this.simpleModalService.addModal(ImageCropperComponent).subscribe((data) => {
+      this.portraitImage = data;
+    });
+  }
+  showBlog(index: any) {
+    this.simpleModalService.addModal(ImageCropperComponent).subscribe((data) => {
+      this.articleImage[index] = data;
+      ((this.articleForm.get('article') as FormArray).at(index) as FormGroup).get('image').patchValue(data);
+    });
+  }
 
 }
