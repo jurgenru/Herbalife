fc1b8470a1cc52beb1e1a3489a2bf6a9e6df125c
Noelia Martínez <noeowo384@gmail.com>
2021-09-09 16:00:41 -0400

    blog: View sin artículos

--- src/app/pages/blog/view/view.component.ts
+++ src/app/pages/blog/view/view.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { BlogService } from 'src/app/services/blog.service';
 @Component({
   selector: 'app-blog-view',
   templateUrl: './view.component.html',
-  styleUrls: ['./view.component.scss']
+  styleUrls: ['./view.component.css']
 })
 export class ViewComponent implements OnInit {
 
-  constructor() { }
+  blog: any = {};
+  articles: any;
+
+  constructor(
+    private blogService: BlogService,
+  ) {
+    this.blogService.getById(1).subscribe(res => {
+      this.blog = res;
+      console.log(this.blog);
+    })
+  }
 
   ngOnInit(): void {
   }
