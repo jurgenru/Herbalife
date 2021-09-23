d3e383231efd90ba328560da3cad1e57d23f5975
Noelia Martínez <noeowo384@gmail.com>
2021-09-23 16:48:04 -0400

    stage:store - rescatando datos para formulario

--- src/app/stage/store/delivery/delivery.component.ts
+++ src/app/stage/store/delivery/delivery.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CartService } from 'src/app/services/cart.service';
+import { ManagerService } from 'src/app/services/manager.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-delivery',
@@ -8,16 +11,59 @@ import { CartService } from 'src/app/services/cart.service';
 })
 export class DeliveryComponent implements OnInit {
   buy: boolean = false;
-  public grandTotal: number = 0;
-
-  constructor(private cartService: CartService) { }
+  order: any = {};
+  userData: any
+  constructor(
+    private cartService: CartService,
+    private userService: UserService,
+    private managerService: ManagerService,
+    private formBuilder: FormBuilder,
+    ) { 
+      this.get();
+    }
 
   ngOnInit(): void {
-    this.grandTotal = this.cartService.getTotalPrice();
+    this.createForm();
   }
 
-  btnBuy(){
-    this.buy = true;
+  createForm(){
+    this.order = this.formBuilder.group({
+      names: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      nit: ['', Validators.required],
+      status: ['', Validators.required],
+      country: ['', Validators.required],
+      city: ['', Validators.required],
+      address: ['', Validators.required],
+    })
+  }
+
+  get(){
+    this.userService.me().subscribe((user:any) => {
+      this.managerService.getByUserId(user.id).subscribe((data: any) => {
+        data.forEach(element => {
+          this.userData = element;
+          console.log(element);
+        });
+        this.order.value.names = this.userData.names;
+        this.order.value.country = this.userData.country
+        this.order.value.city = this.userData.city;
+        this.order.value.address = this.userData.address;
+        this.order.value.email = user.email;
+        this.order.value.userId = user.id;
+        this.order.value.total = this.cartService.getTotalPrice();
+      })
+    })
+  }
+
+  post(){
+    this.order.value.productId =  [];
+    JSON.parse(localStorage.getItem('cartList')).forEach(element => {
+      this.order.value.productId.push({"id":element.id, "quantity":element.quantity, "total": element.total});
+    });
+
+    console.log('post', this.order.value);
+    // this.buy = true;
   }
 
   buying(){
