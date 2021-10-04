6571acd949d605c4cf0053f6c2279b335e6d303f
Sergio Mallea <sergiomallea80@gmail.com>
2021-10-04 16:55:29 -0400

    se muestra una lista de las actividades realizadas

--- src/app/stage/customer/activity/activity.component.ts
+++ src/app/stage/customer/activity/activity.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { ServiceService } from 'src/app/services/service.service';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 
 @Component({
   selector: 'app-activity',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./activity.component.css']
 })
 export class ActivityComponent implements OnInit {
-
-  constructor() { }
+  inscriptions: any = [];
+  index: number = 0;
+  constructor(
+    private userService: UserService,
+    private serviceService: ServiceService,
+    private spinner: NgxUiLoaderService,
+  ) { }
 
   ngOnInit(): void {
+    this.getInscription();
   }
-
+  getInscription() {
+    var previousId = null;
+    const start = new Date();
+    this.spinner.start();
+    const filter = `{"fields": {"id": true, "title": true, "image": true}}`;
+    this.userService.me().subscribe((data: any) => {
+      this.userService.getInscriptionById(data.id).subscribe((ins: any) => {
+        ins.forEach(element => {
+          this.serviceService.getById(element.serviceId, filter).subscribe(serviceData => {
+            const end = new Date();
+            const elapsed = (end.getSeconds() - start.getSeconds()) * 1000;
+            setTimeout(() => {
+              if (element.serviceId != previousId) {
+                this.inscriptions[this.index] = serviceData;
+                console.log(this.inscriptions[this.index]);
+                this.index++;
+                previousId = element.serviceId;
+              }
+              this.spinner.stop();
+            }, elapsed);
+            // if(element.serviceId != previousId){
+            //   this.inscriptions[this.index]=serviceData;
+            //   console.log(this.inscriptions[this.index]);
+            //   this.index++;
+            //   previousId = element.serviceId;
+            // }
+          });
+        });
+      });
+    });
+  }
 }
