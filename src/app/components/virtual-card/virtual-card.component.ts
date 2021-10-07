19d217c7a5380dfd9f3f2d472dc7e3d331b714e4
Noelia Martínez <noeowo384@gmail.com>
2021-10-07 12:25:15 -0400

    virtual-card: creando archivos en componente -virtual card-

---
+++ src/app/components/virtual-card/virtual-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-virtual-card',
+  templateUrl: './virtual-card.component.html',
+  styleUrls: ['./virtual-card.component.css']
+})
+export class VirtualCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
