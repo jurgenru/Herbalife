5775efb5747b3f039c93ceb6170cd55c713d5813
Noelia Martínez <noeowo384@gmail.com>
2021-10-06 23:14:43 -0400

    shopping: añadiendo datos en tabla -mis compras-

--- src/app/stage/customer/shopping/shopping.component.ts
+++ src/app/stage/customer/shopping/shopping.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from 'src/app/services/user.service';
+import { ProductService } from 'src/app/services/product.service';
 @Component({
   selector: 'app-shopping',
   templateUrl: './shopping.component.html',
   styleUrls: ['./shopping.component.css']
 })
 export class ShoppingComponent implements OnInit {
+  order: any = {};
+  product: any = [];
+  constructor( 
+    private userService: UserService,
+    private productService: ProductService
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void { this.get(); }
 
-  ngOnInit(): void {
+  get(){
+    this.userService.me().subscribe((user:any)=>{
+      const filter = `{"fields": {"id": true, "total": true, "productId": true}, "order":["id DESC"]}`;
+      this.userService.getOrderById(user.id, filter).subscribe((res:any) =>{
+        res.forEach(element => {
+          this.order = element;
+          JSON.parse(this.order.productId).forEach(prod => {
+            this.productService.getById(prod.id).subscribe((res:any)=>{
+              this.product.push({'id': res.id, 'name': res.name, 'image': res.image, 'quantity': prod.quantity, 'total': prod.total});
+            })
+          });
+        });
+      })
+    })
   }
 
 }
