1a62a6ce4c302f216c8a059ade074387b61e158b
Sergio Mallea <sergiomallea80@gmail.com>
2021-10-08 18:51:24 -0400

    se creo la nueva ruta para virtual card

--- src/app/virtual-card/virtual-card.component.ts
+++ src/app/virtual-card/virtual-card.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./virtual-card.component.scss']
 })
 export class VirtualCardComponent implements OnInit {
+  data: any = {};
+  type: number=1;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.get()
   }
-
+  get(){
+    this.data = JSON.parse(localStorage.getItem('virtual-card'));
+    this.data.socialMedia = JSON.parse(this.data.socialMedia)
+    console.log(this.data);
+  }
 }
+
